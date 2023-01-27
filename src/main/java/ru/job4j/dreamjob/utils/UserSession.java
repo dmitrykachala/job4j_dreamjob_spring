bf931bbef966a49959897c9e4c5c3f3ac8cb22ee
dmitrykachala <kosas@mail.ru>
2023-01-27 08:59:53 +0300

    2. HttpSession [#504864 #384513]

--- src/main/java/ru/job4j/dreamjob/utils/UserSession.java
+++ src/main/java/ru/job4j/dreamjob/utils/UserSession.java
@@ -6,22 +6,17 @@ import ru.job4j.dreamjob.model.User;
 import javax.servlet.http.HttpSession;
 
 public final class UserSession {
-    private Model model;
-    private HttpSession session;
 
-    private UserSession(Model model, HttpSession session) {
-        this.model = model;
-        this.session = session;
+    private UserSession() {
+    }
+
+    public static void of(Model model, HttpSession session) {
         var user = (User) session.getAttribute("user");
         if (user == null) {
             user = new User();
             user.setName("Гость");
         }
         model.addAttribute("user", user);
-    }
-
-    public static UserSession of(Model model, HttpSession session) {
-        return new UserSession(model, session);
     }
 
 }
